bb767c108d3cc77687dfe9886813f9f3cece6d78
unknown <lh.pedrosot@hotmail.com>
2020-03-19 18:56:50 -0300

    create new in router

--- solution/src/Router.js
+++ solution/src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { Login, Home, Logout } from './Pages/index'
+import { Login, Home, Logout, CreateNew } from './Pages/index'
 
 import MainLayout from './Layout/MainLayout'
 
@@ -9,6 +9,7 @@ const Routes = () => (
     <Switch>
       <MainLayout exact path="/login" component={Login} />
       <MainLayout exact path="/" component={Home} />
+      <MainLayout exact path="/new" component={CreateNew} />
 			<Route exact path="/logout" component={Logout} />
       <Route component={() => <div>Page not Found</div>} />
     </Switch>
