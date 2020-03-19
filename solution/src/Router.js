1958a5ee6c73dd8ddb6030eb81d39844757a74a3
unknown <lh.pedrosot@hotmail.com>
2020-03-19 18:05:13 -0300

    adding login component to router

--- solution/src/Router.js
+++ solution/src/Router.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Login } from './Pages/index'
 
 import MainLayout from './Layout/MainLayout'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <MainLayout exact path="/login" component={''} />
+      <MainLayout exact path="/login" component={Login} />
       <Route component={() => <div>Page not Found</div>} />
     </Switch>
   </BrowserRouter>
